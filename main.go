de15a06ce46e3575b07539ca32bdb41833e317c3
Michael Rybka <mikerybka@users.noreply.github.com>
2025-12-03 12:52:19 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,49 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 	"time"
 )
 
 func main() {
 	buildScriptRepoPath := os.Getenv("BUILD_SCRIPT_REPO_PATH")
 	if buildScriptRepoPath == "" {
-		buildScriptRepoPath = "/var/lib/src/build"
+		buildScriptRepoPath = "/var/src/build"
 	}
 
 	for {
-		updated, err := pullBuildScript(buildScriptRepoPath)
+		err := pullBuildScript(buildScriptRepoPath)
 		if err != nil {
-			fmt.Println("pull error:", err)
+			panic(err)
 		}
 
-		if updated {
-			fmt.Println("starting build")
-			err = build(buildScriptRepoPath)
-			if err != nil {
-				fmt.Println("build error:", err)
-			}
+		err = build(buildScriptRepoPath)
+		if err != nil {
+			panic(err)
 		}
 
 		time.Sleep(time.Hour)
 	}
 }
 
-func pullBuildScript(buildScriptRepoPath string) (bool, error) {
+func pullBuildScript(buildScriptRepoPath string) error {
 	cmd := exec.Command("git", "pull")
 	cmd.Dir = buildScriptRepoPath
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		os.Stdout.Write(out)
-		return false, err
+		return err
 	}
-	if strings.TrimSpace(string(out)) == "Already up to date." {
-		return false, nil
-	}
-	return true, nil
+	return nil
 }
 
 func build(buildScriptRepoPath string) error {
